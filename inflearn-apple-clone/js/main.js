4ac0bfb121451c1e9a11521032c0d43c3d97f768
saseungmin <dbd02169@naver.com>
2021-10-27 00:08:04 +0900

    [Update] Apple Clone 현재 활성시킬 씬 결정하기
    - 현재 활성시킬 씬 결정하기

--- inflearn-apple-clone/js/main.js
+++ inflearn-apple-clone/js/main.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-param-reassign */
 (() => {
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
 
   const sceneInfo = [
     {
@@ -45,6 +48,23 @@
   };
 
   function scrollLoop() {
+    prevScrollHeight = 0;
+
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) { // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+        return;
+      }
+
+      currentScene--;
+    }
   }
 
   window.addEventListener('resize', setLayout);
